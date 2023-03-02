c9c3d7a65034d80ea5485423d90ea31c610525a3
“aliakwe” <aliakwe@yahoo.com>
2023-03-02 12:46:36 +0100

    fix app not rendering in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRoute from "./routes";
+
 function App() {
-  return <AppRoute />;
+  return (
+    <>
+      <BrowserRouter>
+        <AppRoute />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
