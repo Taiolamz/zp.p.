da998860704e3de6d771dd2cc05f24aa7f406ffc
“aliakwe” <aliakwe@yahoo.com>
2023-03-29 02:04:23 +0100

    implement reusable clipboard

---
+++ src/components/clipboard/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { colors, spacing, borderRadius } from "../../utils";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  align-self: flex-start;
+  cursor: copy;
+  background-color: transparent;
+`;
